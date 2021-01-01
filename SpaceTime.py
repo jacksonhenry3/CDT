a49f5cedfba92695c0d7b71cbd5af388a3b5657f
Jackson Henry <jacksonhenry3@gmail.com>
2020-12-31 20:22:45 -0500

    Added faces contain node dict

--- SpaceTime.py
+++ SpaceTime.py
@@ -1,3 +1,8 @@
+"""
+Trying to use node and face NOT vertex and simplex
+"""
+
+
 import numpy as np
 import Display
 
@@ -12,6 +17,7 @@ class SpaceTime(object):
         self.nodes = []  # nodes is just a list of indicies
         self.node_x = {}  # a dict with node indices as keys
         self.node_t = {}  # a dict with node indices as keys
+        self.faces_containing = {}
 
         self.faces = []  # faces is a list of node index tuples
         self.face_dilaton = {}  # a dict with keys of face tuples
@@ -53,6 +59,7 @@ class SpaceTime(object):
                 #These are the faces (a different two can be chosen, the only important thing is that they are uniquly defined by the vertex (t,x))
                 f1 = frozenset({index, right, future_right})
                 f2 = frozenset({index, left, past_left})
+                    
                 self.faces.append(f1)
                 self.faces.append(f2)
 
@@ -61,31 +68,51 @@ class SpaceTime(object):
                 self.face_dilaton[f2] = 1
 
                 #This defines the two spatialy adjacent simplices to f1
-                self.face_x[f1] = [
-                    frozenset({index, future_right, future}),
-                    frozenset(
-                        {right, future_right, future_start + (index + 2) % space_size}
-                    ),
-                ]
+                f1_l = frozenset({index, future_right, future})
+                f1_r = frozenset({right, future_right, future_start + (index + 2) % space_size})
+                self.face_x[f1] = [f1_l,f1_r]
                 
                 #This defines the two spatialy adjacent simplices to f2
-                self.face_x[f2] = [
-                    frozenset({index, past, past_left}),
-                    frozenset({left, past_left, past_start + (index - 2) % space_size}),
-                ]
+                f2_l = frozenset({index, past, past_left})
+                f2_r = frozenset({left, past_left, past_start + (index - 2) % space_size})
+                self.face_x[f2] = [f2_l,f2_r]
                 
                 #These are the faces in the future of f1 and f2
-                self.face_t[f1] = frozenset({index, right, past})
-                self.face_t[f2] = frozenset({index, left, future})
+                f1_t = frozenset({index, right, past})
+                self.face_t[f1] = f1_t
+                f2_t = frozenset({index, left, future})
+                self.face_t[f2] = f2_t
+                
+                self.faces_containing[index] = {f1,f2,f1_l,f2_l,f1_t,f2_t}
                 index += 1
+                
+    def get_faces_containing(self,n):
+        #get all simplices that contain a particular vertex 
+        return({face for face in self.faces if n in face})
 
     def move(self, node, future, past):
-        pass
+        """
+        A move should add one node and 2 simplices. we can pop all the structures to be modified out of the dicts and then push them back in once they've been modified. This mean we need to know what could get modfified in any given move.
+        """
+        
+        
+        print()
 
 
 a = SpaceTime()
-a.generate_flat(16, 16)
-
-
-
+a.generate_flat(12, 12)
+#Display.show_simplex_adjacency_matrix(a)
 Display.show_vertex_adjacency_matrix(a)
+a.move(10,0,0)
+
+print(a.get_faces_containing(23)==a.faces_containing[23])
+print()
+print()
+
+
+#this checks if in a flat space-time each node belongs to 6 simplices
+for n in a.nodes:
+    if not (a.get_faces_containing(n)==a.faces_containing[n]):
+       #pass
+       print("Fail")
+print('pass')
