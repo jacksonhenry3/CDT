76125a96ab93ce2ee3456c03cc9cd1875cdfe5f0
Jackson Henry <jackson.henry3@gmail.com>
2021-02-03 21:17:59 -0500

    better naive coordinates

--- Display.py
+++ Display.py
@@ -2,6 +2,22 @@ import numpy as np
 from numpy import arctan2, cos, sin
 from math import pi
 import matplotlib.pyplot as plt
+import math
+import random
+
+# utility functions
+def angular_seperation(theta_1, theta_2):
+    res = -(((theta_1 - theta_2) + pi) % (2 * pi) - pi)
+    if math.isnan(res):
+        print()
+        print(theta_1, theta_2)
+        print("FAILED")
+        quit()
+    return res
+
+
+# Adjacency matrices
+# these need to be revamped
 
 
 def show_face_adjacency_matrix(st):
@@ -86,6 +102,9 @@ def show_node_adjacency_matrix(st):
     plt.show()
 
 
+# coordinate definitions
+
+
 def get_naive_coords(st):
     """
     Returns two dicts with (space angle , time angle)
@@ -95,11 +114,12 @@ def get_naive_coords(st):
     T = len(layers)
     theta_x = []
     theta_t = []
-
+    shift = -np.repeat(np.arange(0, T), 2)
     for t, layer in enumerate(layers):
         N = len(layer)
         # print(N)
-        theta_x.append(np.arange(N) / N * 2 * pi)
+        # theta_x.append(np.roll(np.arange(N), -(t)) / N * 2 * pi - pi / N * (t % 2))
+        theta_x.append(np.roll(np.arange(N), shift[t]) / N * 2 * pi + pi / N * (-t % 2))
         theta_t.append(np.full(N, t) / T * 2 * pi)
     theta_x = theta_x
     theta_t = theta_t
@@ -115,10 +135,8 @@ def get_naive_coords(st):
     return (theta_x_dict, theta_t_dict)
 
 
-import math
-
-
-def get_smart_coords(st):
+def get_preserve_x_length_coords(st):
+    """returns x and t angles where spatial seperation is constant and the overall rotation of a spatial section is designed to minimize tim-like edge-lengths"""
     theta_x, theta_t = get_naive_coords(st)
     layers = st.get_layers()
 
@@ -126,8 +144,7 @@ def get_smart_coords(st):
         layer_total = 0
         for n in layer:
             for connection in st.node_all_connections(n):
-                delta = theta_x[connection] - theta_x[n]
-                delta = (delta + pi) % (2 * pi) - pi
+                delta = angular_seperation(theta_x[connection], theta_x[n])
                 layer_total += delta
         layer_avg = layer_total / len(layer)
         print(layer_avg)
@@ -137,8 +154,83 @@ def get_smart_coords(st):
     return (theta_x, theta_t)
 
 
-def angular_seperation(theta_1, theta_2):
-    return -(((theta_1 - theta_2) + pi) % (2 * pi) - pi)
+def get_x_spring_coords(st, iterations=1000, k=0.1, b=1, dt=0.01):
+    """returns x and t angles where each nodes edges are treated like spatial springs. I.E. the time-like layers are still regularly seperated but the x-position should minimize the total connection length."""
+    theta_x, theta_t = get_naive_coords(st)
+    theta_x_tmp = theta_x
+    v = {n: 0 for n in st.nodes}
+
+    # one iteration
+    # random.shuffle
+    for i in range(iterations):
+        # print(i)
+
+        for layer in st.get_layers():
+            for n in layer:
+                delta = 0
+                for connection in st.node_all_connections(n):
+                    t1 = theta_x[n]
+                    t2 = theta_x[connection]
+                    delta -= angular_seperation(t1, t2) / pi
+
+                a = -k * delta - b * v[n] ** 2
+                # for spatial_connection in st.node_x(n):
+                #     t1 = theta_x[n]
+                #     t2 = theta_x[spatial_connection]
+                #     delta = angular_seperation(t1, t2) / pi
+                #     a += 0.1 / (delta * len(layer)) ** 2
+            print(a, delta)
+            v[n] += a * dt
+            theta_x_tmp[n] += v[n] * dt
+            theta_x_tmp[n] = theta_x_tmp[n] % (2 * pi)
+
+        theta_x = theta_x_tmp
+
+    return (theta_x, theta_t)
+
+
+def get_x_avg_coords(st, iterations=100):
+    """returns x and t angles where each nodes edges are treated like spatial springs. I.E. the time-like layers are still regularly seperated but the x-position should minimize the total connection length."""
+    theta_x, theta_t = get_naive_coords(st)
+
+    # for n in st.nodes:
+
+    return (theta_x, theta_t)
+
+
+def get_smart_coords(st, iterations=100):
+    """returns x and t angles where each nodes edges are treated like spatial springs. I.E. the time-like layers are still regularly seperated but the x-position should minimize the total connection length."""
+    theta_x, theta_t = get_naive_coords(st)
+    theta_x_tmp = theta_x
+    v = {n: 0 for n in st.nodes}
+
+    # one iteration
+    # random.shuffle
+    for i in range(iterations):
+        print(i)
+        t = 0
+        for layer in st.get_layers():
+            print()
+            print(t)
+            print()
+            t += 1
+
+            for n in layer:
+                delta = 0
+                for connection in st.node_all_connections(n):
+                    t1 = theta_x[n]
+                    t2 = theta_x[connection]
+                    delta += angular_seperation(t1, t2)
+                print(delta)
+                theta_x_tmp[n] += delta / (len(layer) * 10)
+                theta_x_tmp[n] = theta_x_tmp[n] % (2 * pi)
+
+        theta_x = theta_x_tmp
+
+    return (theta_x, theta_t)
+
+
+# 3d plots
 
 
 def plot_3d_torus(st, shading=None):
@@ -148,8 +240,8 @@ def plot_3d_torus(st, shading=None):
             "wireframe": True,
             "wire_width": 100,
             "wire_color": "white",  # Wireframe rendering
-            "width": 600,
-            "height": 600,  # Size of the viewer canvas
+            "width": 300,
+            "height": 300,  # Size of the viewer canvas
             "antialias": True,  # Antialising, might not work on all GPUs
             "scale": 2.0,  # Scaling of the model
             "side": "DoubleSide",  # FrontSide, BackSide or DoubleSide rendering of the triangles
@@ -162,7 +254,7 @@ def plot_3d_torus(st, shading=None):
             "point_color": "red",
             "point_size": 0.01,  # Point properties of overlay points
         }
-    theta_x, theta_t = get_smart_coords(st)
+    theta_x, theta_t = get_naive_coords(st)
 
     import numpy as np
     from numpy import sin, cos
@@ -205,6 +297,7 @@ def plot_3d_torus(st, shading=None):
     )
 
 
+# cutting a time slice
 def plot_3d_cyl(st, shading=None):
     # remove the ignored time triangles! This only works for spetial geometries.
 
@@ -228,7 +321,7 @@ def plot_3d_cyl(st, shading=None):
             "point_color": "red",
             "point_size": 0.01,  # Point properties of overlay points
         }
-    theta_x, theta_t = get_smart_coords(st)
+    theta_x, theta_t = get_x_spring_coords(st)
 
     import numpy as np
     from numpy import sin, cos
@@ -256,8 +349,8 @@ def plot_3d_cyl(st, shading=None):
             (c + 0.01 * cos(u + pi)) * sin(v),
         )
         # coords[n] = (a * cos(v), a * cos(v), u * 10)
-        if len(st.node_all_connections(n)) - 6 != 0:
-            print(len(st.node_all_connections(n)) - 6)
+        # if len(st.node_all_connections(n)) - 6 != 0:
+        #     print(len(st.node_all_connections(n)) - 6)
         colors.append((len(st.node_all_connections(n)) - 6))
         idx += 1
     v = []
@@ -294,97 +387,12 @@ def plot_3d_cyl(st, shading=None):
     )
 
 
-def plot_3d_cyl_SDFIKSJDBFSJDIFB(st, shading=None):
-    # remove the ignored time triangles! This only works for spetial geometries.
-
-    if shading is None:
-        shading = {
-            "flat": True,  # Flat or smooth shading of triangles
-            "wireframe": True,
-            "wire_width": 0.001,
-            "wire_color": "#222",  # Wireframe rendering
-            "width": 600,
-            "height": 600,  # Size of the viewer canvas
-            "antialias": True,  # Antialising, might not work on all GPUs
-            "scale": 1.5,  # Scaling of the model
-            "side": "DoubleSide",  # FrontSide, BackSide or DoubleSide rendering of the triangles
-            "colormap": "Spectral",
-            "normalize": [None, None],  # Colormap and normalization for colors
-            "background": "#222",  # Background color of the canvas
-            "line_width": 1.0,
-            "line_color": "black",  # Line properties of overlay lines
-            "bbox": False,  # Enable plotting of bounding box
-            "point_color": "red",
-            "point_size": 0.01,  # Point properties of overlay points
-        }
-    theta_x, theta_t = get_smart_coords(st)
-
-    import numpy as np
-    from numpy import sin, cos
-    import meshplot as mp
-    import random
-
-    mp.offline()
-
-    c = 3
-    a = 1
-    idx = 0
-    idx_to_node = {}
-    node_to_idx = {}
-    coords = {}
-    colors = []
-    for n in st.nodes:
-        idx_to_node[idx] = n
-        node_to_idx[n] = idx
-        v = theta_x[n]
-        u = theta_t[n]
-
-        coords[n] = (
-            v,
-            u,
-            0,
-        )
-
-        colors.append((len(st.node_all_connections(n)) - 6))
-        idx += 1
-    v = []
-    f = []
-    c = []
-    for i in range(idx):
-        n = idx_to_node[i]
-        v.append(coords[n])
-
-    for face in st.faces:
-        col = 0
-        mapped_face = []
-
-        for node in face:
-            n = node
-            mapped_face.append(node_to_idx[node])
-            col += len(st.node_all_connections(n)) - 6
-        arbitraryN = next(iter(face))
-
-        # doesnt add triangles that span the inside of the cyl
-        if all([abs(theta_t[n] - theta_t[arbitraryN]) < pi for n in face]):
-            if all([abs(theta_x[n] - theta_x[arbitraryN]) < pi for n in face]):
-                f.append(mapped_face)
-
-    # mp.plot(np.array(v), filename="plots/test", shading={"point_size": 3})
-    v = np.array(v)
-    f = np.array(f)
-    p = mp.plot(
-        v,
-        f,
-        c=np.array(colors),
-        filename="plots/test",
-        shading=shading,
-        return_plot=True,
-    )
+# 2d plots (cutting a space and time slice)
 
 
 def plot_2d(st, offeset=0):
     # theta_x, theta_t = get_smart_coords_old(st)
-    theta_x, theta_t = get_smart_coords(st)
+    theta_x, theta_t = get_naive_coords(st)
     #
     import numpy as np
     from numpy import sin, cos
@@ -506,4 +514,5 @@ def plot_2d(st, offeset=0):
     # plt.legend(
     #     ("nodes", "Triangles", "node connections"), loc="upper right", shadow=True
     # )
+    plt.axis("off")
     plt.show()
