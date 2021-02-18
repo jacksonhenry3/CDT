bbcb0b22f33dfb2ab0abf35fdcb3102c12e4ece8
JWKennington <jameswkennington@gmail.com>
2021-02-18 16:04:15 -0500

    initial carve out of setattr and getattr

--- cdtea/event.py
+++ cdtea/event.py
@@ -6,24 +6,33 @@ sugar for code readability
 import typing
 from collections import Iterable
 
-PASS_THRU_ATTRS = {
+
+class PassThruAttr:
+    """Constants class for pass thru attributes"""
+    Left = 'left'
+    Right = 'right'
+    Past = 'past'
+    Future = 'future'
+    Faces = 'faces'
+
+
+PASS_THRU_ATTR_MAP = {
     # Mapping of attribute name in Node object and corresponding lookup-dict in SpaceTime object
     # NOTE: this depends on implementation details of SpaceTime class, and should be updated in tandem
-    'left': 'node_left',
-    'right': 'node_right',
-    'past': 'node_past',
-    'future': 'node_future',
-    'faces': 'faces_containing'
+    PassThruAttr.Left: 'node_left',
+    PassThruAttr.Right: 'node_right',
+    PassThruAttr.Past: 'node_past',
+    PassThruAttr.Future: 'node_future',
+    PassThruAttr.Faces: 'faces_containing'
 }
 EVENT_RETURNING_ATTRS = ('left', 'right', 'past', 'future')
-EDGE_CONSISTENCY_ATTR_DUALS = {
+EDGE_CONSISTENCY_ATTR_DUALS = [
     # Mapping of attributes that define edge consistency relationships, which
     # may also be viewed as parity and time-reversal duals
-    'left': 'right',
-    'right': 'left',
-    'past': 'future',
-    'future': 'past',
-}
+    (PassThruAttr.Left, PassThruAttr.Right),
+    (PassThruAttr.Past, PassThruAttr.Future),
+]
+EDGE_CONSISTENCY_ATTR_DUALS_DICT = dict(EDGE_CONSISTENCY_ATTR_DUALS + [(v, k) for k, v in EDGE_CONSISTENCY_ATTR_DUALS])
 
 
 class Event:
@@ -34,7 +43,7 @@ class Event:
     """
 
     def __init__(self, space_time, event_key):
-        """Create an Event isntance
+        """Create an Event instance
 
         Args:
             space_time:
@@ -64,23 +73,25 @@ class Event:
         # TODO add "and other.space_time == self.space_time" once __eq__ defined for SpaceTime
         return isinstance(other, Event) and other.key == self.key
 
-    def __getattr__(self, item):
-        """Override the behavior of attribute lookup ONLY for the case of pass-thru attributes,
-        which are defined above in PASS_THRU_ATTRIBUTES. for these attributes only, the getattr
-        call will be redirected to the underlying SpaceTime object from which the key originates
+    def _get_pass_thru_attr_(self, key):
+        """Helper private method for looking up pass-thru attributes.For these attributes only,
+        the call will be redirected to the underlying SpaceTime object from which the key originates
 
         Args:
-            item:
+            key:
                 str, the name of the attribute to get
+
+        Returns:
+            Event, List[Event], or List[frozenset] depending on whether a single neighbor,
+            multiple neighbors, or faces are requested
         """
-        if item in PASS_THRU_ATTRS:
-            value = getattr(self.space_time, PASS_THRU_ATTRS[item])[self.key]
-            if item in EVENT_RETURNING_ATTRS:
+        if key in PASS_THRU_ATTR_MAP:
+            value = getattr(self.space_time, PASS_THRU_ATTR_MAP[key])[self.key]
+            if key in EVENT_RETURNING_ATTRS:
                 if isinstance(value, Iterable):
                     return [v if v is None else Event(space_time=self.space_time, event_key=v) for v in value]
                 return value if value is None else Event(space_time=self.space_time, event_key=value)
             return value
-        return super(Event, self).__getattribute__(item)
 
     def __hash__(self):
         """Make Event hashable
@@ -97,70 +108,41 @@ class Event:
         # TODO update this to include a time coordinate if possible
         return 'Event(ST{:d}, {:d})'.format(len(self.space_time.nodes), -1 if self.key is None else self.key)
 
-    def __setattr__(self, key, value):
-        """Override the behavior of attribute setting ONLY for the case of pass-thru attributes,
-        which are defined above in PASS_THRU_ATTRIBUTES. for these attributes only, the setattr
-        call will be redirected to the underlying SpaceTime object from which the key originates
+    @property
+    def left(self):
+        """Pass-thru accessor for left neighbor
 
-        Args:
-            key:
-                str, the name of the attribute to set
-            value:
-                Any, if an Event instance and key is a pass-thru attr, value will be coerced to int before
-                assignment to corresponding SpaceTime attribute lookup dict
-
-        Notes:
-            Edge Consistency:
-                The below code ensures that both ends of edges are maintained in a way that
-                preserves the following consistency relations between events A and B:
-
-                    (1) A.right = B    <==>  A = B.left
-                    (2) A.left = B     <==>  A = B.right
-                    (3) B in A.past    <==>  A in B.future
-                    (4) B in A.future  <==>  A in B.past
-
-                In order to maintain relations (3) and (4), the original value of the attribute
-                assigned to the given key must be tracked, so as to know which event to update
-                after the new attribute assignment.
+        Returns:
+            Event, the left neighbor
         """
-        if key not in PASS_THRU_ATTRS:
-            return super().__setattr__(key, value)
+        return self._get_pass_thru_attr_(PassThruAttr.Left)
 
-        # Assign new value
-        value_key = [event_key(v) for v in value] if isinstance(value, Iterable) else event_key(value)
-        original_value = getattr(self, key)
-        getattr(self.space_time, PASS_THRU_ATTRS[key])[self.key] = value_key
+    @property
+    def right(self):
+        """Pass-thru accessor for right neighbor
 
-        if original_value is None or original_value == []:
-            # Short-circuit if the original value is None (this corresponds to a empty-reference)
-            return
+        Returns:
+            Event, the right neighbor
+        """
+        return self._get_pass_thru_attr_(PassThruAttr.Right)
 
-        if key not in EDGE_CONSISTENCY_ATTR_DUALS:
-            # TODO define behavior for faces and face consistency
-            return
+    @property
+    def past(self):
+        """Pass-thru accessor for past neighbors
 
-        # Curate remaining values for edge-consistency
-        if isinstance(value, Iterable):
-            # Update the edge-consistency dual-attribute of the newly assigned event
-            new_events = [n for n in value if n not in original_value]
-            for n in new_events:
-                if self.key not in getattr(n.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[n.key]:
-                    getattr(n.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[n.key].append(self.key)
+        Returns:
+            List[Event], the past neighbors
+        """
+        return self._get_pass_thru_attr_(PassThruAttr.Past)
 
-            # Update the edge-consistency dual-attribute of the replaced event
-            # TODO must decide about replaced edge behavior
-            # replaced_events = [n for n in original_value if n not in value]
-            # for r in replaced_events:
-            #     if self.key in getattr(r.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[r.key]:
-            #         getattr(r.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[r.key].remove(self.key)
-        else:
-            # Update the edge-consistency dual-attribute of the newly assigned event
-            if getattr(value, EDGE_CONSISTENCY_ATTR_DUALS[key]) != self:
-                setattr(value, EDGE_CONSISTENCY_ATTR_DUALS[key], self)
+    @property
+    def future(self):
+        """Pass-thru accessor for future neighbors
 
-            # Update the edge-consistency dual-attribute of the replaced event
-            # TODO we need to think about this behavior, the replaced event has no natural replaced attribute?
-            # getattr(original_value.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[original_value.key] = None
+        Returns:
+            List[Event], the future neighbors
+        """
+        return self._get_pass_thru_attr_(PassThruAttr.Future)
 
     @property
     def spatial_neighbors(self):
@@ -212,7 +194,7 @@ def event_key(e: typing.Union[Event, int]) -> int:
     return e.key if isinstance(e, Event) else e
 
 
-def events(space_time, keys: typing.Union[int, typing.Iterable[int]]) -> typing.Union[Event, typing.List[Event]]:
+def events(space_time, keys: typing.Union[int, typing.Iterable[int]] = None) -> typing.Union[Event, typing.List[Event]]:
     """Helper function for creating multiple Event instances from an iterable
     of SpaceTime keys
 
@@ -227,9 +209,77 @@ def events(space_time, keys: typing.Union[int, typing.Iterable[int]]) -> typing.
     """
     if isinstance(space_time, Iterable):  # TODO more thorough check in case we make spacetime iterable..
         return list(zip(*[events(st, keys) for st in space_time]))
+    if keys is None:
+        keys = space_time.nodes
     if isinstance(keys, Iterable):
         return [Event(space_time=space_time, event_key=k) for k in keys]
     return Event(space_time=space_time, event_key=keys)
+
+
+def connect(self, key, value):
+    """Override the behavior of attribute setting ONLY for the case of pass-thru attributes,
+    which are defined above in PASS_THRU_ATTRIBUTES. for these attributes only, the setattr
+    call will be redirected to the underlying SpaceTime object from which the key originates
+
+    Args:
+        key:
+            str, the name of the attribute to set
+        value:
+            Any, if an Event instance and key is a pass-thru attr, value will be coerced to int before
+            assignment to corresponding SpaceTime attribute lookup dict
+
+    Notes:
+        Edge Consistency:
+            The below code ensures that both ends of edges are maintained in a way that
+            preserves the following consistency relations between events A and B:
+
+                (1) A.right = B    <==>  A = B.left
+                (2) A.left = B     <==>  A = B.right
+                (3) B in A.past    <==>  A in B.future
+                (4) B in A.future  <==>  A in B.past
+
+            In order to maintain relations (3) and (4), the original value of the attribute
+            assigned to the given key must be tracked, so as to know which event to update
+            after the new attribute assignment.
+    """
+    if key not in PASS_THRU_ATTR_MAP:
+        return super().__setattr__(key, value)
+
+    # Assign new value
+    value_key = [event_key(v) for v in value] if isinstance(value, Iterable) else event_key(value)
+    original_value = getattr(self, key)
+    getattr(self.space_time, PASS_THRU_ATTR_MAP[key])[self.key] = value_key
+
+    if original_value is None or original_value == []:
+        # Short-circuit if the original value is None (this corresponds to a empty-reference)
+        return
+
+    if key not in EDGE_CONSISTENCY_ATTR_DUALS:
+        # TODO define behavior for faces and face consistency
+        return
+
+    # Curate remaining values for edge-consistency
+    if isinstance(value, Iterable):
+        # Update the edge-consistency dual-attribute of the newly assigned event
+        new_events = [n for n in value if n not in original_value]
+        for n in new_events:
+            if self.key not in getattr(n.space_time, PASS_THRU_ATTR_MAP[EDGE_CONSISTENCY_ATTR_DUALS[key]])[n.key]:
+                getattr(n.space_time, PASS_THRU_ATTR_MAP[EDGE_CONSISTENCY_ATTR_DUALS[key]])[n.key].append(self.key)
+
+        # Update the edge-consistency dual-attribute of the replaced event
+        # TODO must decide about replaced edge behavior
+        # replaced_events = [n for n in original_value if n not in value]
+        # for r in replaced_events:
+        #     if self.key in getattr(r.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[r.key]:
+        #         getattr(r.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[r.key].remove(self.key)
+    else:
+        # Update the edge-consistency dual-attribute of the newly assigned event
+        if getattr(value, EDGE_CONSISTENCY_ATTR_DUALS[key]) != self:
+            setattr(value, EDGE_CONSISTENCY_ATTR_DUALS[key], self)
+
+        # Update the edge-consistency dual-attribute of the replaced event
+        # TODO we need to think about this behavior, the replaced event has no natural replaced attribute?
+        # getattr(original_value.space_time, PASS_THRU_ATTRS[EDGE_CONSISTENCY_ATTR_DUALS[key]])[original_value.key] = None
 
 
 class GluingPoint(int):
