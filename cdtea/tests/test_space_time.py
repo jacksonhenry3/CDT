83b60e3a2e3cc7eff5ef4d00705008ee5b267b0c
JWKennington <jameswkennington@gmail.com>
2021-02-21 21:21:00 -0500

    add diff tools

--- cdtea/tests/test_space_time.py
+++ cdtea/tests/test_space_time.py
@@ -1,9 +1,12 @@
 """Unittests for SpaceTime"""
+import collections
+import itertools
 import os
 import pathlib
 import tempfile
 
 import networkx
+import pandas
 import pytest
 
 from cdtea import event, modifications, space_time
@@ -32,6 +35,57 @@ def find_duplicate_temporal_connections(st: SpaceTime):
     return duplicates if duplicates != [] else None
 
 
+def ranges(i):
+    for a, b in itertools.groupby(enumerate(i), lambda t: t[1] - t[0]):
+        b = list(b)
+        yield b[0][1], b[-1][1]
+
+
+# TODO move these tools somewhere real eventually
+DiffSummary = collections.namedtuple('DiffSummary', 'unique_left unique_right common diffs')
+
+def spacetime_diff(st1: SpaceTime, st2: SpaceTime, display_results: bool = False):
+    """Collect difference between shared nodes in two spacetimes"""
+    common_nodes = st1.nodes.intersection(st2.nodes)
+    unique_nodes_1 = set(n for n in st1.nodes if n not in common_nodes)
+    unique_nodes_2 = set(n for n in st2.nodes if n not in common_nodes)
+    diffs = []
+    for n in common_nodes:
+        if st1.node_left[n] != st2.node_left[n]:
+            diffs.append((n, 'left', st1.node_left[n], st2.node_left[n]))
+        if st1.node_right[n] != st2.node_right[n]:
+            diffs.append((n, 'right', st1.node_right[n], st2.node_right[n]))
+        if st1.node_past[n] != st2.node_past[n]:
+            diffs.append((n, 'past', st1.node_past[n], st2.node_past[n]))
+        if st1.node_future[n] != st2.node_future[n]:
+            diffs.append((n, 'future', st1.node_future[n], st2.node_future[n]))
+    diffs = pandas.DataFrame([[n, t, str(l), str(r)] for n, t, l, r in diffs], columns=['Node', 'Type', 'Left', 'Right'])
+    summary = DiffSummary(unique_nodes_1, unique_nodes_2, common_nodes, diffs)
+    if not display_results:
+        return summary
+    print(format_diff(summary))
+
+
+def format_diff(summary: DiffSummary) -> str:
+    """Utility for formatting diff information"""
+    # TODO fix indenting
+    return """SpaceTime Diff Summary:
+
+{ulc:d} Unique L Nodes: {unique_left}
+{urc:d} Unique R Nodes: {unique_right}
+{cc:d} Common Nodes: {common_nodes}
+
+Diff Table:
+{table}
+    """.format(ulc=len(summary.unique_left),
+               unique_left=str(summary.unique_left),
+               urc=len(summary.unique_right),
+               unique_right=str(summary.unique_right),
+               cc=len(summary.common),
+               common_nodes=str(list(ranges(list(sorted(summary.common))))),
+               table=summary.diffs.to_string(index=False))
+
+
 class TestTestingUtils:
     """Tests for the helper test functions"""
 
@@ -51,6 +105,26 @@ class TestTestingUtils:
         invalid_dups = find_duplicate_temporal_connections(invalid_cdt)
         assert invalid_dups is not None
         assert invalid_dups == [(0, 'future', [10, 19, 19])]
+
+    def test_ranges(self):
+        """Test ranges utility"""
+        rs = list(ranges([0, 1, 2, 3, 4, 7, 8, 9, 11]))
+        assert rs == [(0, 4), (7, 9), (11, 11)]
+
+    def test_diff(self):
+        """Test diff tool"""
+        cdt1 = space_time.generate_flat_spacetime(3, 3)
+        cdt2 = space_time.generate_flat_spacetime(3, 4)
+        diff = spacetime_diff(cdt1, cdt2)
+        assert isinstance(diff, DiffSummary)
+
+    def test_diff_summary(self):
+        """Test diff tool summary"""
+        cdt1 = space_time.generate_flat_spacetime(3, 3)
+        cdt2 = space_time.generate_flat_spacetime(3, 4)
+        diff = spacetime_diff(cdt1, cdt2)
+        summary = format_diff(diff)
+        assert isinstance(summary, str)
 
 
 class TestSpaceTime:
@@ -87,6 +161,15 @@ class TestSpaceTime:
         st.push(st.pop([node]))
         dups = find_duplicate_temporal_connections(st)
         assert dups is None
+
+    def test_push_pop_identity(self):
+        """Test that push * pop == identity"""
+        base  = space_time.generate_flat_spacetime(10, 10) # to compare against
+        st = space_time.generate_flat_spacetime(10, 10)
+        assert base == st
+        node = event.Event(st, 13)
+        st.push(st.pop([node]))
+        assert base == st
 
 
 class TestSpaceTimeSerialize:
