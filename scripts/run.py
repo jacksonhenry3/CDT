db119a96b1390f70a52f8514223388a38a50b04e
JWKennington <jameswkennington@gmail.com>
2021-02-24 16:11:39 -0500

    fix run for sets

--- scripts/run.py
+++ scripts/run.py
@@ -11,21 +11,22 @@ def main():
     FST = generate_flat_spacetime(size, size)
     random.seed(9230)
     #
-    # for i in range(10):
-    #     if i % 100:
-    #         print(i)
-    #     n = FST.get_random_node()
-    #     f = random.choice(n.future)
-    #     p = random.choice(n.past)
-    #     move(FST,n, f, p)
-    n = FST.get_random_node()
-    print(n)
-    imove(FST,n)
+    for i in range(50):
+        if i % 100:
+            print(i)
+        n = FST.get_random_node()
+        f = random.choice(list(n.future))
+        p = random.choice(list(n.past))
+        move(FST,n, f, p)
+    # n = FST.get_random_node()
+    # print(n)
+    # imove(FST,n)
 
     # FST.imove(n)
     print("plottin")
     # display.get_naive_coords(FST)
-    plot_st.plot_3d(FST,type = "cylinder")
+    plot_st.plot_2d(FST)
+    # plot_st.plot_3d(FST,type = "cylinder")
 
 
 if __name__ == '__main__':  # import shielding
