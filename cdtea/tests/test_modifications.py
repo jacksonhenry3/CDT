ea07a1411d0ec61b4af6989366fa866bd15bc66c
JWKennington <jameswkennington@gmail.com>
2021-02-24 15:55:56 -0500

    cleanup test modifications module, verify that multiple moves are working

--- cdtea/tests/test_modifications.py
+++ cdtea/tests/test_modifications.py
@@ -1,7 +1,7 @@
 """Tests for the modifications module"""
 from cdtea import event, modifications
-from cdtea.space_time import SpaceTime
-from cdtea.tests.test_space_time import dummy_space_time, find_gluing_point_references
+from cdtea.space_time import SpaceTime, generate_flat_spacetime
+from cdtea.tests import audit
 
 
 class TestModifications:
@@ -9,32 +9,29 @@ class TestModifications:
 
     def test_move(self):
         """Test move"""
-        dst = dummy_space_time(3, 3)
+        dst = generate_flat_spacetime(3, 3)
         n, f, p = event.events(dst, [4, 7, 1])
         modifications.move(dst, n, f, p)
         assert isinstance(dst, SpaceTime)
 
     def test_multiple_moves(self):
-        dst = dummy_space_time(4, 4)
+        dst = generate_flat_spacetime(4, 4)
         n1, f1, p1 = event.events(dst, [12, 0, 11])
         modifications.move(dst, n1, f1, p1)
         n2, f2, p2 = event.events(dst, [13, 1, 9])
-        gpn, gpr = find_gluing_point_references(dst)
-        subspace = dst.pop([n2])
-        gpn2, gpr2 = find_gluing_point_references(subspace)
         modifications.move(dst, n2, f2, p2)
         assert isinstance(dst, SpaceTime)
 
     def test_imove(self):
         """Test inverse move"""
-        dst = dummy_space_time(4, 4)
+        dst = generate_flat_spacetime(4, 4)
         n = event.Event(dst, 6)
         modifications.imove(dst, n)
         assert isinstance(dst, SpaceTime)
 
     def test_functional_inverses(self):
-        dst = dummy_space_time(3, 3)
-        dst_copy = dummy_space_time(3, 3)
+        dst = generate_flat_spacetime(3, 3)
+        dst_copy = generate_flat_spacetime(3, 3)
         n, f, p = event.events(dst, [4, 7, 1])
         modifications.move(dst, n, f, p)
         new_node = event.Event(dst, 9)
