bc8b6b230a96a8c587cf2ef8cc780307f926928b
JWKennington <jameswkennington@gmail.com>
2021-03-06 13:37:03 -0500

    add some timing harness to run

--- scripts/run.py
+++ scripts/run.py
@@ -1,4 +1,5 @@
 import random
+import time
 
 from cdtea.visualization import plot_st
 from cdtea.space_time import SpaceTime
@@ -7,25 +8,28 @@ from cdtea.moves import increase,decrease
 from cdtea.event import Event
 def main():
 
-    size = 40
+    size = 1000
     FST = generate_flat_spacetime(size, size)
     random.seed(9230)
-    #
-    for i in range(50):
-        if i % 100:
+    start = time.perf_counter()
+    for i in range(20):
+        if i % 100 == 0:
             print(i)
         n = FST.get_random_node()
         f = random.choice(list(n.future))
         p = random.choice(list(n.past))
         increase(FST, n, f, p)
+
+    stop = time.perf_counter()
+    print('Time: {:.4f}'.format(stop - start))
     # n = FST.get_random_node()
     # print(n)
     # imove(FST,n)
 
     # FST.imove(n)
-    print("plottin")
+    # print("plottin")
     # display.get_naive_coords(FST)
-    plot_st.plot_2d(FST)
+    # plot_st.plot_2d(FST)
     # plot_st.plot_3d(FST,type = "cylinder")
 
 
